4621fc9f5a42d2e7e383624d810a743211b31244
Timothy Reen <reentim@gmail.com>
2019-10-15 13:51:25 +1100

    Move some comments to a text file

--- lib/index.ts
+++ lib/index.ts
@@ -1,24 +1,2 @@
-// TODO
-// keyword arguments would be nice
-// interfaces
-// default arguments for constructors
-
-// what about, since I'm so into space, a space game? would also suit lowbit
-// graphics
-// could be lander & shooter
-// with bearing -- like Pirates!
-// full fucking Homeworld
-//
-// takes on other games, Stronghold with more economics
-//
-// Actually: lander shooter thing
-// orbital mechanics like osmsos
-// some lander stuff
-// some homeworld stuff
-//
-// or narrow it down to lander game, but you start off as the lunar module,
-// have to do all the stuff
-
-import { Game } from './game.js'
-
+import { Game } from './game'
 ;(window as any)['g'] = <Game>new Game()
